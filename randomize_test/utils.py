ceb4526dea1ae9e8b0f63c726bc8c23aebb57447
Giovanni Spadaccini <giospadaccini74@gmail.com>
2024-06-01 22:29:42 +0200

    change the generator

--- randomize_test/utils.py
+++ randomize_test/utils.py
@@ -8,7 +8,7 @@ POSSIBILE_REQ_DEPENDENCY = ['latency', 'availability']
 fake = Faker()
 
 def generate_dep(name,old_val=None, is_node=False):
-    if name =='ram':
+    if name =='ram'
         min_val = 1 if old_val is None else old_val  
         max_val = 10 if not is_node else 30 
         return fake.random_int(min=min_val, max=max_val)
@@ -18,15 +18,15 @@ def generate_dep(name,old_val=None, is_node=False):
         return fake.random_int(min=min_val, max=max_val)
     elif name == 'storage':
         min_val = 800 if old_val is None else old_val  
-        max_val = 1000 if not is_node else 2500
+        max_val = 1000 if not is_node else 4500
         return fake.random_int(min=min_val, max=max_val)
     elif name == 'bwIn':
         min_val = 50 if old_val is None else old_val  
-        max_val = 100 if not is_node else 250
+        max_val = 100 if not is_node else 450
         return fake.random_int(min=min_val, max=max_val)
     elif name == 'bwOut':
         min_val = 50 if old_val is None else old_val  
-        max_val = 100 if not is_node else 250
+        max_val = 100 if not is_node else 450
         return fake.random_int(min=min_val, max=max_val)
     elif name == 'security':
         k= 1 if random.random() < 0.1 else 0
